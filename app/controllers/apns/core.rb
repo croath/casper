4558b0b2e0f4987e4563f2758b360da8ffaf584d
croath <croathliu@gmail.com>
2014-08-25 11:59:17 +0800

    separate files and classes

--- app/controllers/apns/core.rb
+++ app/controllers/apns/core.rb
@@ -153,33 +153,3 @@ class PushConnection
     PushNotificaion.new_with_json(json)
   end
 end
-
-class ConnectionService
-  @@pool_count = 3
-  @@conns = Array.new
-
-  # def self.current_connection
-  #   PushConnection.instance
-  # end
-
-  def self.random_connection
-    @@conns[Random.rand(@@pool_count-1)]
-  end
-
-  def self.internal_push(alert, token, badge, params)
-    p = PushNotification.new_with_infos(token, alert, badge, params)
-    conn = self.random_connection
-    conn.save_to_store(p)
-    conn.send_push([p])
-  end
-
-  for i in 0..(@@pool_count-1)
-    conn = PushConnection.new(i, true)
-    conn.connect
-    @@conns << conn
-  end
-
-  def self.send_push(alert, token, badge, params)
-    self.internal_push(alert, token, badge, params)
-  end
-end
